6e003335747b33f897a7360aa7e0ed37c87741a6
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-29 00:55:02 +0100

    Update 8-uppercase.py

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -3,6 +3,7 @@ def uppercase(str):
     for char in str:
         if char.islower():
             temp = ord(char)
-            print("{} ".format(chr(temp - 32)), end='')
+            print("{}".format(chr(temp - 32)), end='')
         else:
             print(char, end=' ')
+    print("\n")
