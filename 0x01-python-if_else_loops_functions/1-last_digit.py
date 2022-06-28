bad6e4fa76083ae303b94452125a343497888089
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-06-28 21:20:34 +0100

    Update 1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,12 +2,16 @@
 import random
 number = random.randint(-10000, 10000)
 # the get_last function below gets the last digit of the number
+
+
 def get_last(num):
     temp = str(num)
     last = int(temp[len(temp)-1])
     if num < 0:
         return last * -1
     return last
+
+
 last_num = get_last(number)
 if last_num > 5:
     print(f"Last digit of {number} is {last_num} and is greater than 5")
