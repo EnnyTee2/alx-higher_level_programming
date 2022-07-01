e47776e27a3ed55840d7c5dcdd0b01e9cf9c480f
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-01 01:06:37 +0100

    Create 2-args.py

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """This program prints the number of and the list of its arguments"""
+    import sys
+
+    index = 1
+    counter = len(argv)
+    if counter == 1:
+        print("{} argument:".format(counter))
+        print("{index}: {}".format(argv[counter - 1]))
+    elif counter == 0:
+        print("{} arguments.".format(counter))
+    else:
+        print("{} arguments:".format(counter))
+        while index <= counter:
+            print("{}: {}".format(index, argv[index-1]))
+            index += 1
