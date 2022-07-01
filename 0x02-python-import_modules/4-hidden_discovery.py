8c908a34d13f7b1f11914a8d86c9c6cf33018070
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-01 01:59:34 +0100

    Create 4-hidden_discovery.py

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+if __name__ == "__main__":
+    """a program that prints all the names defined by the compiled module hidden_4.pyc"""
+
+    import sys, hidden_4
+    names = dir(hidden_4)
+    for name in names:
+        if name.startswith('__'):
+            continue
+        else:
+            print(name)
