2f972fc05ac48ebad535c251591f522c82cd345b
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-02 22:54:12 +0100

    Update 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -5,7 +5,11 @@ of a list, in reverse order."""
 
 
 def print_reversed_list_integer(my_list=[]):
-    count = len(my_list)-1
+    for integer in reversed(mylist):
+        print(integer)
+
+""" Alternate method 
+    count = len(my_list) - 1
     while count >= 0:
         print("{:d}".format(my_list[count]))
-        count -= 1
+        count -= 1    """
