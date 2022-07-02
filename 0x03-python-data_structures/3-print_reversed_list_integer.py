a6625c6375b89f60444f1e47f72fd53320aa99bb
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-02 23:15:34 +0100

    Update 3-print_reversed_list_integer.py

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -8,7 +8,8 @@ def print_reversed_list_integer(my_list=[]):
     for integer in reversed(my_list):
         print("{:d}".format(integer))
 
-""" Alternate method 
+
+""" Alternate method
     count = len(my_list) - 1
     while count >= 0:
         print("{:d}".format(my_list[count]))
