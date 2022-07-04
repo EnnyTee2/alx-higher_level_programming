2ca689edc2d6aad3530a3894cddf37e3340754f1
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-04 23:22:39 +0100

    Create 10-divisible_by_2.py

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""a function that finds all multiples of 2 in a list."""
+
+
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for x in my_list:
+        if !(x % 2):
+            new_list.append(True)
+        new_list.append(False)
+    return new_list
