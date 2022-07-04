4d78bcdbac014dc82bc9d06e70f726d37fc71497
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-04 13:06:49 +0100

    Update 7-add_tuple.py

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -4,7 +4,22 @@
 
 
 def add_tuple(tuple_a=(), tuple_b=()):
-    a1 = tuple_a[0]
+    for i in range(len(tuple_a)):
+        if i < 1:
+            try:
+                a1 = (tuple_a[i] + tuple_b[i])
+            except:
+                a1 = (tuple_a[i] + 0)
+        else:
+            try:
+                a2 = (tuple_a[i] + tuple_b[i])
+            except:
+                a2 = (tuple_a[i] + 0)
+    return (a1, a2)
+
+
+
+    """a1 = tuple_a[0]
     a2 = tuple_a[1]
     b1 = 0
     b2 = 0
@@ -22,4 +37,4 @@ def add_tuple(tuple_a=(), tuple_b=()):
             b2 = tuple_b[1]
         except:
             pass
-    return (a1 + b1, a2 + b2)
+    return (a1 + b1, a2 + b2)""""
