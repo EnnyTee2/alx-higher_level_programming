066b6e06360358e9283e3eaf60d715c77aa681eb
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-06 16:04:11 +0100

    Update 0-square_matrix_simple.py

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,7 +5,7 @@ value of all integers of a matrix."""
 
 
 def square_matrix_simple(matrix=[]):
-    new_mat = matrix[:]
+    new_mat = [i[:] for i in matrix]
     for x in range(len(new_mat)):
         for y in range(len(new_mat[x])):
             new_mat[x][y] *= new_mat[x][y]
