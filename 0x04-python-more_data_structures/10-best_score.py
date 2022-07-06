1b2a3fc07f02b4c1e9cc7ba658e93ddbb601948d
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-06 19:01:04 +0100

    Update 10-best_score.py

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,7 @@
 
 def best_score(a_dictionary):
     best_ = None
-    if a_dictionary is None:
+    if a_dictionary is None or len(a_dictionary) == 0:
         return best_
     best = sorted(list(a_dictionary.values()))[-1]
     for key in a_dictionary.keys():
