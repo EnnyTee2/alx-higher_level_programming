07d07f97c32bab661bc2d79168415ab2b4a1a422
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-06 16:16:24 +0100

    Update 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -8,7 +8,4 @@ def search_replace(my_list, search, replace):
     for index in range(len(my_list)):
         if my_list[index] == search:
             my_list[index] = replace
-            break
-        else:
-            continue
     return my_list
