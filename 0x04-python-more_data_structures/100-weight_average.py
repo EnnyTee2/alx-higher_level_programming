fc0159b422dafd18b6c203a215fa149cd7e42474
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-06 23:21:38 +0100

    Update 100-weight_average.py

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,7 @@ average of all integers tuple (<score>, <weight>)"""
 
 
 def weight_average(my_list=[]):
-    wa_sum = 1
+    wa_sum = 0
     div = 0
     for score, weight in my_list:
         wa_sum += (score * weight)
