f90a10639818a3460cf029b21c01124315494fa1
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-07 01:49:21 +0100

    Update 102-complex_delete.py

--- 0x04-python-more_data_structures/102-complex_delete.py
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -5,9 +5,8 @@ a specific value in a dictionary."""
 
 
 def complex_delete(a_dictionary, value):
-    for key in a_dictionary.keys():
-        if dict[key] == value:
+    for key, values in a_dictionary.items():
+        if values == value:
             a_dictionary.pop(key, None)
-        else:
             continue
     return a_dictionary
