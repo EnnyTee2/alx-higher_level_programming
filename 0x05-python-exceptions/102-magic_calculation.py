c358375082dbb936ee1fe202eebcb87de90f5bde
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-18 23:00:38 +0100

    Update 102-magic_calculation.py

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -3,15 +3,15 @@
 """python function to produce given bytecode"""
 
 
-def magic_calculation(a, b):
+def magic_calculation(a,b):
     result = 0
+
     for i in range(1, 3):
         try:
-            if i > a:
+            if (i > a):
                 raise Exception('Too far')
-            else:
-                result += a ** b / i
-        except ValueError:
+            result += a ** b / i
+        except Exception:
             result = b + a
             break
     return result
