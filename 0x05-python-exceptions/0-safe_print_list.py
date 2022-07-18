b288d203bfb0f79502f611dbd439abc234980bad
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-18 11:46:14 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,8 +5,7 @@
 
 def safe_print_list(my_list=[], x=0):
     try:
-        for index in range(0, x+1):
+        for index in range(0, x):
             print(my_list[index], end='')
     except Exception:
         error = Exception
-            
