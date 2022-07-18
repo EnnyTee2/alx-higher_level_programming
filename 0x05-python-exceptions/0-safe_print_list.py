c8373a858cd9499696c6127f6913c6b0ac1c2143
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-18 11:52:06 +0100

    Update 0-safe_print_list.py

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -4,8 +4,12 @@
 
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
     try:
         for index in range(0, x):
             print(my_list[index], end='')
+            count += 1
     except Exception:
         error = Exception
+    finally:
+        print(f'\nnb_print: {count}')
