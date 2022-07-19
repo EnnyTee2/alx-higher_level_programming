1226f11d721ca7239060feb896b1bf64b5c6f086
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-19 23:32:41 +0100

    Update 4-square.py

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -4,7 +4,7 @@
 square by: (based on 3-square.py)
 
 Instantiation with optional parameter
-size: def __init__(self, size=0): 
+size: def __init__(self, size=0):
 size must be an integer, otherwise raise a
 TypeError exception with the message size must be an integer
 
@@ -21,9 +21,11 @@ def area(self): that returns the current square area"""
 
     def area(self):
         return self.__size * self.__size
+    
     @property
     def size(self):
         return self.__size
+    
     @size.setter
     def size(self, value):
         if type(value) is int:
