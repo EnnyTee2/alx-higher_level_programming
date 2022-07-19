7af75bcde6be59d57a851ff2b6960869997398af
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-19 23:39:37 +0100

    Create 5-square.py

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+
+"""a class Square that defines a
+square by: (based on 4-square.py)
+
+Instantiation with optional parameter
+size: def __init__(self, size=0):
+size must be an integer, otherwise raise a
+TypeError exception with the message size must be an integer
+
+if size is less than 0, raise a ValueError exception
+with the message size must be >= 0"""
+
+
+class Square:
+    """Includes a Public instance method:
+def area(self): that returns the current square area"""
+
+    def __init__(self, size=0):
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) is int:
+            if value < 0:
+                raise ValueError("size must be >= 0")
+            self.__size = value
+        else:
+            raise TypeError("size must be an integer")
+ 
+    def area(self):
+        return self.__size * self.__size
+    
+    def my_print(self):
+        if self.__size == 0:
+            print("")
+        else:
+            for i in range(self.__size):
+                print("#"*self.__size)
