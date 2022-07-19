2180df198dd6d06d1d4f8325a6749fba5b2561e2
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-19 22:36:22 +0100

    Update 3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -7,11 +7,14 @@ square by: (based on 2-square.py)"""
 class Square:
     """Instantiation with optional parameter
     size: def __init__(self, size=0):
+
 size must be an integer, otherwise raise a TypeError 
 exception with the message size must be an integer
+
 if size is less than 0, raise a ValueError exception
 with the message size must be >= 0
-    Includes a Public instance method: 
+
+Includes a Public instance method: 
 def area(self): that returns the current square area"""
     
     def __init__(self, size=0):
@@ -20,6 +23,7 @@ def area(self): that returns the current square area"""
                 raise ValueError("size must be >= 0")
             self.__size = size
         else:
-             raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
+
     def area(self):
         return self.__size * self.__size
