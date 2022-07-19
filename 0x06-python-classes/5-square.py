69938af9ed9d3870de20088dc0e99fabbf469c03
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-19 23:42:18 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -31,10 +31,10 @@ def area(self): that returns the current square area"""
             self.__size = value
         else:
             raise TypeError("size must be an integer")
- 
+
     def area(self):
         return self.__size * self.__size
-    
+
     def my_print(self):
         if self.__size == 0:
             print("")
