3002c9e60ec6f35feb2f44bed874e6f8408a4b31
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-19 22:20:29 +0100

    Update 2-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -14,4 +14,4 @@ Instantiation with optional size: def __init__(self, size=0):"""
                 raise ValueError("size must be >= 0")
             self.__size = size
         else:
-             raise TypeError("size must be an integer")
+            raise TypeError("size must be an integer")
