2548ec017ded0962d9319c962ce7c599d367aee9
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-19 23:57:42 +0100

    Update 5-square.py

--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,20 +1,24 @@
 #!/usr/bin/python3
 
 """a class Square that defines a
-square by: (based on 4-square.py)
+square (based on 4-square.py) with private instance attributes
+and a property which is accessed and changed
+by a getter and setter respectively"""
 
-Instantiation with optional parameter
-size: def __init__(self, size=0):
-size must be an integer, otherwise raise a
-TypeError exception with the message size must be an integer
-
-if size is less than 0, raise a ValueError exception
-with the message size must be >= 0"""
 
 
 class Square:
-    """Includes a Public instance method:
-def area(self): that returns the current square area"""
+    """"
+    Square (class): creates a square with the size specified by
+    the parameter size and raise error if type is not int or
+    size is less than zero.
+    Attributes:
+        size (int): specify the size of the square.
+        area(): returns the area of the square.
+        my_print(): prints out the square with # character to stdout.
+    Args:
+        size (int): size of the square.
+    """
 
     def __init__(self, size=0):
         self.size = size
@@ -33,11 +37,14 @@ def area(self): that returns the current square area"""
             raise TypeError("size must be an integer")
 
     def area(self):
+        """Calculates area of the square and return the result"""
+
         return self.__size * self.__size
 
     def my_print(self):
+        """ Prints the square with the character # to the stdout. """
+
         if self.__size == 0:
-            print("")
-        else:
-            for i in range(self.__size):
-                print("#"*self.__size)
+            print()
+        for i in range(self.__size):
+            print("#" * self.__size)
