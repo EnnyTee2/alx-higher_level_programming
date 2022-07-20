a4e4565fc39b91c4b1045c06a786953b4d9571fd
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-20 05:04:04 +0100

    Update 101-square.py

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -77,3 +77,5 @@ class Square:
                 print("\n" * self.__position[1], end="")
             for i in range(self.__size):
                 print(" " * self.__position[0] + "#" * self.__size)
+    def __str__(self):
+        return my_print(self)
