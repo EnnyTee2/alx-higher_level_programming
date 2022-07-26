c1d5afacf0dd4cd8cf4c64d28e2ff9c4a3213727
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-26 05:41:58 +0100

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -71,7 +71,7 @@ class Rectangle:
         Args:
             size (int): The width and height of the new Rectangle.
         """
-        return Rectangle(size, size)
+        return cls(size, size)
 
     def __str__(self):
         """Returns the printable representation of the Rectangle.
