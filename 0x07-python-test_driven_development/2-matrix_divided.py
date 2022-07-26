577ace2e4ae7897596bf0c6c5937ec8d923313f6
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-26 02:03:57 +0100

    Update 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -35,9 +35,9 @@ def matrix_divided(matrix, div):
                                 "must have the same size"))
             else:
                 for y in range(0, len(matrix[x])):
-                    if type(matrix[x][y]) == int or \
-                    type(matrix[x][y]) == float:
-                        new_matrix[x][y] = round((matrix[x][y]/div), 2)
+                    if (type(matrix[x][y]) == int or
+                        type(matrix[x][y]) == float):
+                            new_matrix[x][y] = round((matrix[x][y]/div), 2)
                     else:
                         raise TypeError("matrix must be a matrix"
                                         "(list of lists) of integers/floats")
