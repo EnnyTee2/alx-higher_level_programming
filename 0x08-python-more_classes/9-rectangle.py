a7dd16ea4044a71376bef4266c410d996ecc53e8
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-26 05:35:07 +0100

    Update 9-rectangle.py

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -64,14 +64,14 @@ class Rectangle:
         if rect_1.area() >= rect_2.area():
             return rect_1
         return rect_2
-    
+
     @classmethod
     def square(cls, size=0):
         """Return a new Rectangle with width and height equal to size.
         Args:
             size (int): The width and height of the new Rectangle.
         """
-        return (cls(size, size))
+        return cls(size, size)
 
     def __str__(self):
         """Returns the printable representation of the Rectangle.
