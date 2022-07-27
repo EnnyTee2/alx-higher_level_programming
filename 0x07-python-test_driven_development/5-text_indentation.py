63ace4f656b0746740a9976d01cbb40c0e4ad56f
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-07-28 00:18:20 +0100

    Update 5-text_indentation.py

--- 0x07-python-test_driven_development/5-text_indentation.py
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -1,1 +1,20 @@
+#!/usr/bin/python3
 
+def text_indentation(text):
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+    temp = ''
+    cont = []
+    for letter in text:
+        temp += letter
+        if letter == '?' or letter == ':' or letter == '.':
+            cont.append(temp)
+            temp = ''
+    cont.append(temp)
+    final = [tex.lstrip() for tex in cont]
+    last = final[-1:]
+    for each in final:
+        if each == last[0]:
+            print(each, end='')
+        else:
+            print(each, end='\n\n')
