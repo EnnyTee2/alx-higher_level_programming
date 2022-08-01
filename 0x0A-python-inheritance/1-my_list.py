83e8bccb5a3c1f183d54f7d488c68daa6678cce7
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-02 00:32:19 +0100

    Update 1-my_list.py

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
-""" a class MyList that inherits from list"""
+"""a class MyList that inherits from list"""
 
 
 class MyList(list):
     """prints the list, but sorted (ascending sort)"""
+    
     def print_sorted(self):
+        """Print a list in sorted ascending order."""
         print(sorted(self))
