3c0fb9c30af63c87bbb4da70372850811c8ff275
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-01 23:05:37 +0100

    Update 11-square.py

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,7 +1,15 @@
 #!/usr/bin/python3
+"""Square implementation based on Rectangle class
+    with __str__ method overriding"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
 
 class Square(Rectangle):
-    
+    """ private instance attr size is the size of the square
+        the __str__ method is overridden"""
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
