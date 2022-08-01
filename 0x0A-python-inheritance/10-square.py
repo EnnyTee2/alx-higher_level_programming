07af90d1f0ac5aa297991178b539d24974f2ac34
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-01 22:47:41 +0100

    Update 10-square.py

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,8 +1,16 @@
 #!/usr/bin/python3
+"""Implements a Square based on Rectangle class"""
+
+
 Rectangle = __import__('9-rectangle').Rectangle
 
+
 class Square(Rectangle):
-    
+    """
+    Args:
+        size - size of square
+    """
+
     def __init__(self, size):
         self.integer_validator("size", size)
         self.__size = size
