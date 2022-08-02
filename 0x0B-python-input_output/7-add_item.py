d3c1720bb177f4f7729889f4d1edd4c99356cd5c
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-03 00:51:26 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""#!/usr/bin/python3
 
-"""a script that adds all arguments to a
-Python list, and then save them to a file:"""
+a script that adds all arguments to a
+Python list, and then save them to a file:
 import sys
 
 
@@ -19,4 +20,24 @@ if __name__ == "__main__":
             save_to_json_file(self.arg_list, filename)
 
             self.arg_list = load_from_json_file(filename)
-    a = Run()
+    a = Run()"""
+import os
+import sys
+import json
+
+
+save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
+
+filename = 'add_item.json'
+args = len(sys.argv)
+
+if not os.path.isfile(filename):
+    with open(filename, 'w', encoding='utf-8') as f:
+        f.write('[]')
+
+if args > 1:
+    data = load_from_json_file(filename)
+    for i in range(1, args):
+        data.append(sys.argv[i])
+    save_to_json_file(data, filename)
