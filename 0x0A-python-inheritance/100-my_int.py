39aceaf4d964228ceca7e272ecf3d97f06791643
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-02 01:57:17 +0100

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -4,7 +4,7 @@
 
 class MyInt(int):
     """this clas has its neq and eq operators inverted"""
-    
+
     def __neq__(self, value):
         """override parent __neq__ method"""
         super().__neq__(value)
