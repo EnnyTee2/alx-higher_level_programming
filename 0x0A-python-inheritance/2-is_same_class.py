6585be3102c33eae1f0c743a2f1f7301ba8ea995
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-02 01:11:45 +0100

    Update 2-is_same_class.py

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -1,4 +1,11 @@
 #!/usr/bin/python3
 
+#!/usr/bin/python3
+"""Compare if Exact same object"""
+
+
 def is_same_class(obj, a_class):
+    """a function that returns True if the object is exactly
+    an instance of the specified class; otherwise False"""
+    
     return type(obj) is a_class
