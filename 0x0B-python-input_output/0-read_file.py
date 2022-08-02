017213af219470a7d7ae7a1329940fa1f23acbea
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-02 22:38:58 +0100

    Update 0-read_file.py

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,4 +5,6 @@ and prints it to stdout:"""
 
 
 def read_file(filename=""):
-    with open(filename, 'r', encoding="utf-8") as 
+    """reads the specified file and prints it to stdout"""
+    with open(filename, 'r', encoding="utf-8") as myfile
+    print(myfile)
