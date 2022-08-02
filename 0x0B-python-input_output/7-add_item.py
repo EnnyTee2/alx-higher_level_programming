4b6ba93473714442a501546ffc9899a70e1892c1
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-03 00:23:38 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,24 +2,18 @@
 
 """a script that adds all arguments to a
 Python list, and then save them to a file:"""
-import json
 import sys
 
 
-def save_to_json_file(my_obj, filename):
-    """returns the object from its json representation of the string"""
-    with open(filename, 'w') as myfile:
-        json.dump(my_obj, myfile)
-
-def load_from_json_file(filename):
-    """loads the object from its json representation of the string"""
-    with open(filename) as myfile:
-        return json.load(myfile)
-
-arglist = sys.argv[1:]
+if __name__ == "__main__":
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file = \
+        __import__('6-load_from_json_file').load_from_json_file
 
 filename = "add_item.json"
+try:
+    arglist = load_from_json_file(filename)
+except FileNotFoundError:
+    arglist = []
+arglist.extend(sys.argv[1:])
 save_to_json_file(arglist, filename)
-
-my_list = load_from_json_file(filename)
-print(my_list)
