f40cdc9230748d094bc11548aff535f45cc66e81
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-02 02:13:15 +0100

    Update 101-add_attribute.py

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -1,6 +1,10 @@
 #!/usr/bin/python3
+"""a function that adds a new attribute to an object"""
+
 
 def add_attribute(cls, *args):
+    """Add a new attribute to an object if possible."""
+
     super().cls.__init__(*args)
     for a in args:
         cls.a = None
