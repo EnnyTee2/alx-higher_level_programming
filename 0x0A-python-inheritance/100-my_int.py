fc7ee9e54f2842cf4c24de94b5fa4288c475f7fc
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-02 01:55:27 +0100

    Create 100-my_int.py

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""a class MyInt that inherits from int"""
+
+
+class MyInt(int):
+    """this clas has its neq and eq operators inverted"""
+    
+    def __neq__(self, value):
+        """override parent __neq__ method"""
+        super().__neq__(value)
+        return self == value
+
+    def __eq__(self, value):
+        """override parent __eq__ method"""
+        super().__eq__(value)
+        return self != value
