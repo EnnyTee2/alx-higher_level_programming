8ae5dfb2debde22afeaff4fca75be9b2435a418c
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-02 01:38:50 +0100

    Update 4-inherits_from.py

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -7,4 +7,4 @@ def inherits_from(obj, a_class):
     of a class that inherited (directly or indirectly)
     from the specified class; otherwise False"""
     
-    returns issubclass(type(obj), a_class) and not isinstance(obj, type(obj)) 
+    returns issubclass(type(obj), a_class) and  not isinstance(type(obj), a_class) 
