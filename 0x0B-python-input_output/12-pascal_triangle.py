3d33e7f9ba7ba37789e04f6111fdd2d70b8b0106
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-04 00:48:08 +0100

    Update 12-pascal_triangle.py

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -17,7 +17,7 @@ def pascal_triangle(n):
                 sub = [1]
                 break
             elif x == 1:
-                sub = [1,2,1]
+                sub = [1,1]
                 break
             else:
                 if y == 0 or y == count-1:
@@ -27,3 +27,21 @@ def pascal_triangle(n):
         lister.append(sub)
         count = len(sub) + 1
     return lister
+
+
+"""def pascal_triangle(n):
+    """Represent Pascal's Triangle of size n.
+    Returns a list of lists of integers representing the triangle.
+    """
+    if n <= 0:
+        return []
+
+    triangles = [[1]]
+    while len(triangles) != n:
+        tri = triangles[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangles.append(tmp)
+    return triangles"""
