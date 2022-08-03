70c605899d1f40ddf4da7badd95fe94b073e0750
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-03 21:14:23 +0100

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -17,25 +17,3 @@ if __name__ == "__main__":
         arg_list = []
     arg_list.extend(sys.argv[1:])
     save_to_json_file(arg_list, filename)
-
-"""ALTERNATE METHOD
-import os
-import sys
-import json
-
-
-save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
-load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
-
-filename = 'add_item.json'
-args = len(sys.argv)
-
-if not os.path.isfile(filename):
-    with open(filename, 'w', encoding='utf-8') as f:
-        f.write('[]')
-
-if args > 1:
-    data = load_from_json_file(filename)
-    for i in range(1, args):
-        data.append(sys.argv[i])
-    save_to_json_file(data, filename)"""
