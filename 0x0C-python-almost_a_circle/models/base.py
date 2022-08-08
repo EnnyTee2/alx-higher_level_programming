0affa172a56a55b77587bf084fbf2da3f8102300
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-08 20:31:48 +0100

    Update base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,14 +2,21 @@
 
 class Base:
     """This class forms the base of all other classes for this project.
-    It'll help manage the instance attribute id"""
-
+    It'll help manage the instance attribute id
+    
+    Attributes:
+        __nb_objects (int): The number of instances of Bases
+    """
 
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """Initializes a new base
+        Args:
+            id (int): The identity of the new Base (object)
+        """
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            Base.id = Base.__nb_objects
+            self.id = Base.__nb_objects
