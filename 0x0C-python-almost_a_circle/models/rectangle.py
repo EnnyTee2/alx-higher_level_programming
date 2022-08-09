6643b5eb9b4405aaec2c60ad2a10a6d2b278378b
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-09 02:53:26 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Implements a Rectangle based on the Base class"""
 from models.base import Base
-import sys
 
 class Rectangle(Base):
     """The Rectangle class inherits from the base class
