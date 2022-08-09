5a18f5e8736df70bbb5404df0ae391aa70d88413
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-09 18:06:57 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,7 +25,7 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
-    
+
     def update(self, *args, **kwargs):
         """Update the Rectangle.
         Args:
@@ -39,7 +39,7 @@ class Square(Rectangle):
         if args is not None:
             try:
                 self.id = args[0]
-                self.size= args[1]
+                self.size = args[1]
                 self.x = args[2]
                 self.y = args[3]
             except Exception as e:
