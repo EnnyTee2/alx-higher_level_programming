35463dc5748f7faa8b158f628d2971ff4a107af4
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-09 14:49:58 +0100

    Update rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -103,7 +103,7 @@ class Rectangle(Base):
     def __str__(self):
         """Return the print() and str() representation of the Rectangle."""
         return (
-            f'[Rectangle] ({self.id}) {self.x}/{self.y}\n'
+            f'[Rectangle] ({self.id}) {self.x}/{self.y}'
             f' - {self.width}/{self.height}'
         )
 
