7db2655dd15d654b8d29292cb149d13b4eae8127
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-08-09 18:02:54 +0100

    Update square.py

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -25,9 +25,9 @@ class Square(Rectangle):
     def size(self, value):
         self.width = value
         self.height = value
-
+    
     def update(self, *args, **kwargs):
-        """Update the Square.
+        """Update the Rectangle.
         Args:
             *args (ints): New attribute values.
                 - 1st argument represents id attribute
@@ -36,35 +36,17 @@ class Square(Rectangle):
                 - 4th argument represents y attribute
             **kwargs (dict): New key/value pairs of attributes.
         """
-        if args and len(args) != 0:
-            a = 0
-            for arg in args:
-                if a == 0:
-                    if arg is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = arg
-                elif a == 1:
-                    self.size = arg
-                elif a == 2:
-                    self.x = arg
-                elif a == 3:
-                    self.y = arg
-                a += 1
-
-        elif kwargs and len(kwargs) != 0:
-            for k, v in kwargs.items():
-                if k == "id":
-                    if v is None:
-                        self.__init__(self.size, self.x, self.y)
-                    else:
-                        self.id = v
-                elif k == "size":
-                    self.size = v
-                elif k == "x":
-                    self.x = v
-                elif k == "y":
-                    self.y = v
+        if args is not None:
+            try:
+                self.id = args[0]
+                self.size= args[1]
+                self.x = args[2]
+                self.y = args[3]
+            except Exception as e:
+                error = e
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
 
     def to_dictionary(self):
         """Return the dictionary representation of the Square."""
