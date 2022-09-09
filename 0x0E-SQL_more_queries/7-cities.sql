074733b14396fe8eba98a9916c1385e9ed5f129a
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-09-09 01:47:12 +0100

    Update 7-cities.sql

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -5,5 +5,6 @@ CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (
     id   INT UNIQUE NOT NULL AUTO_INCREMENT,
     state_id INT NOT_NULL,
     name VARCHAR(256) NOT NULL,
-    FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id)
+    FOREIGN KEY(state_id) 
+    REFERENCES hbtn_0d_usa.states(id)
 );
