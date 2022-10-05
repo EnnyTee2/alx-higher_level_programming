d14eaed9b77e8e1222ee322fc304a638aea680d8
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-05 22:16:05 +0100

    Update 3-value_argument.js

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,3 +1,2 @@
 #!/usr/bin/node
-
-process.argv[2] === 'undefined' ? console.log('No argument') : console.log(process.argv[2]);
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
