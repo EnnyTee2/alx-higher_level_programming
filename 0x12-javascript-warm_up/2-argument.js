43288968190d98749d1f1eb7beedd967c05986eb
EnnyTee2 <trusteniola@gmail.com>
2022-10-05 12:41:51 -0700

    updates

--- 0x12-javascript-warm_up/2-argument.js
+++ 0x12-javascript-warm_up/2-argument.js
@@ -3,10 +3,8 @@
 const argv_length = process.argv.length;
 if (argv_length === 2) {
   console.log('No argument');
-}
-else if (argv_length === 3) {
+} else if (argv_length === 3) {
   console.log('Argument found');
-}
-else {
+} else {
   console.log('Arguments found');
-};
+}
