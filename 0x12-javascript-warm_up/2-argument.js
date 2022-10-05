f6df2684ef7a0dbfc27b07904241b5990e726869
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-05 20:27:41 +0100

    Create 2-argument.js

---
+++ 0x12-javascript-warm_up/2-argument.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const argv_length = process.argv.length;
+if (argv_length === 2) {
+  console.log('No argument');
+}
+else if (argv_length === 3) {
+  console.log('Argument found');
+}
+else {
+  console.log('Arguments found');
+};
