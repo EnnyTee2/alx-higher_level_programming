1ed653b98e45f7089518ee6d58ef92eabfb50412
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-10 03:25:02 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -11,3 +11,36 @@ if __name__ == "__main__":
     cur = db_con.cursor()
     cur.execute("SELECT * FROM `states`")
     [print(state) for state in cur.fetchall()]
+
+"""#!/usr/bin/python3
+
+import MySQLdb as mdb
+import sys
+
+try:
+    # We connect to a database and create an
+    # instance of it to be used in our code
+    db_con = mdb.connect(
+        host='localhost',
+        user=sys.argv[1],
+        passwd=sys.argv[2],
+        db=sys.argv[3],
+        port=3306)
+
+    curs = db_con.cursor()  # cursor object for transversing the database
+    curs.execute("SELECT * FROM `states`")  # executing an SQL statement
+
+    # storing the SQL query results (rows extracted from selected table)
+    record = curs.fetchall()
+
+    for row in record:
+        print(row)
+
+except mdb.Error as e:
+    print(f"Error {e.args[0]}: {e.args[1]}")
+    sys.exit(1)  # exit with standard error
+
+finally:
+    if db_con:
+        db_con.close()  # close db connection if it is still open
+"""
