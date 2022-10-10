6d2901775b68cf4a7b696239ae7be9f30a66d60f
EnnyTee2 <44221402+EnnyTee2@users.noreply.github.com>
2022-10-10 03:18:22 +0100

    Update 0-select_states.py

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,18 @@
 #!/usr/bin/python3
+# Lists all states from the database hbtn_0e_0_usa.
+# Usage: ./0-select_states.py <mysql username> \
+#                             <mysql password> \
+#                             <database name>
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db_con = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    cur = db_con.cursor()
+    cur.execute("SELECT * FROM `states`")
+    [print(state) for state in cur.fetchall()]
+
+"""#!/usr/bin/python3
 
 import MySQLdb as mdb
 import sys
@@ -29,3 +43,4 @@ except mdb.Error as e:
 finally:
     if db_con:
         db_con.close()  # close db connection if it is still open
+"""
